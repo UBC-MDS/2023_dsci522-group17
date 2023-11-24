ff90db4752f8cc3d9c4a7e27b10b5869be26f9a6
Jake Barnabe <jake.barnabe@queensu.ca>
2023-11-24 14:53:38 -0800

    Updated function docstring

--- preprocessor.py
+++ preprocessor.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import StandardScaler
 
 # Create the function to modularize the preprocessor block
 def preprocessor(passthrough_feats, numeric_feats):
+    """Takes lists of numeric and passthrough"""
     fifa_preprocessor = make_column_transformer(
     ("passthrough", passthrough_feats),
     (StandardScaler(), numeric_feats), 
@@ -11,12 +12,13 @@ def preprocessor(passthrough_feats, numeric_feats):
     return fifa_preprocessor
 
 # Create test data to test the function
-test_data = pd.DataFrame{
+
+test_data = pd.DataFrame({
     'num_col_1': [1, 2, 3, 4, 5],
     'num_col_2': [6, 7, 8, 9, 10],
     'pass_col_1': ['jake', 'merete', 'simon', 'waleed', 'tiffany'],
     'num_col_3': [100, 200, 300, 400, 500]
-}
+})
 
 #Create test cases
 
