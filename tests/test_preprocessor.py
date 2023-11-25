520f86f09ae151d0493f88e1039ad136e023c95d
Jake Barnabe <jake.barnabe@queensu.ca>
2023-11-24 16:05:55 -0800

    Created tests folder and test_preprocessor.py file for test cases

---
+++ tests/test_preprocessor.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import 
+
+# Create test data to test the function
+
+test_data = pd.DataFrame({
+    'num_col_1': [1, 2, 3, 4, 5],
+    'num_col_2': [6, 7, 8, 9, 10],
+    'pass_col_1': ['jake', 'merete', 'simon', 'waleed', 'tiffany'],
+    'num_col_3': [100, 200, 300, 400, 500]
+})
+
+#Create test cases
+
